3cbb8ce8f72025d0663fd9b514f3eab2bade8396
roeis <roeichow@gmail.com>
2015-07-09 17:47:14 +0800

    update detect touch

---
+++ samples/detect/detect.js
@@ -0,0 +1,26 @@
+/**
+ * name: 
+ * author: roeis
+ * description: 
+ */
+(function(){
+    'use strict';
+
+    //navigator 上输出所有信息，有用
+    //
+    //get a tap事件
+
+    // 输出当前状态
+    // 
+    //
+    //
+    // isIOS
+    // isAndroid
+    // isUC
+    // isQQ
+    // isWeixin
+    // isIphone
+
+
+
+})();
