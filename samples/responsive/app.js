f70907da89a5ca0a73652a2704fd795ad1c062af
roeis <roeichow@gmail.com>
2015-08-19 17:07:42 +0800

    update best pratice demo

---
+++ samples/responsive/app.js
@@ -0,0 +1,13 @@
+/**
+ * --------------------------------------------------------
+ * Copyright (c) 2015 All rights reserved.
+ * @version: 1.0.0
+ * @author: roeis
+ * @description: 
+ * --------------------------------------------------------
+ */
+(function(){
+    'use strict';
+    
+    console.log('app start');
+})();
